f88c7a778bfc8fee70ec505a88352c4043d9e43c
Eric Jobin <ejobin@gmail.com>
2019-08-28 23:15:29 -0400

    preparing for Heroku deployment

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/homeworkdb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/homeworkdb", { useNewUrlParser: true }); //Commenting this out for Heroku Deployment???
+
+//If deployed, use the deployed database, otherwise use local database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/homeworkdb"
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
